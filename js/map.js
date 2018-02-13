27fbcb062d0596721b92ea59731bc312e09053d1
pnoll1 <8444617+pnoll1@users.noreply.github.com>
2018-02-12 16:43:57 -0800

    Added map page, data and updated other pages with nav links to page

---
+++ js/map.js
@@ -0,0 +1,58 @@
+var baselayers, ways, all, controls_layers, mymap, overlays, mapbox;
+//var mymap = L.map('map').setView([47.810, -122.384], 9); 
+
+var mapbox = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,    
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoicG5vbGwiLCJhIjoiY2pkNDNyNmtzMHRtOTMzcWZ0Y2szdzh3eCJ9.g8tszbsYH0bVCcj7v8RAEQ'
+});
+var baselayers = {
+    "Mapbox": mapbox
+}
+var mymap = L.map('map', {
+    center: [47.810, -122.384],
+    zoom: 9,
+    layers: [mapbox]
+}); 
+
+
+$.getJSON('edits.geojson',function (data) {
+    var ways = L.geoJSON(data,{
+    style: function (feature) {
+        return {};
+    }
+    ,
+    pointToLayer: function (feature) {
+    	return false;
+    }
+    });
+    var all = L.geoJSON(data,{
+    style: function (feature) {
+        return {};
+    }
+    });
+    ways.addTo(mymap);
+    all.addTo(mymap);
+    var overlays = {
+    "all": all,
+    "ways": ways
+    };
+
+    var controls_layers = L.control.layers(baselayers, overlays);
+    controls_layers.addTo(mymap);
+});
+var legend = L.control({position: 'bottomright'});
+legend.onAdd = function (mymap) {
+
+    var div = L.DomUtil.create('div', 'legend');
+    div.innerHTML = 'OSM Edits'
+        //items = ['Markers', 'Ways'],
+        //labels = [<i style='background'>L.marker()</i>, 'Areas, Buildings, Streets'];
+
+
+    return div;
+};
+
+legend.addTo(mymap);
+                    
